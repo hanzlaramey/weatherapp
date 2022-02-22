e1406d2416f4373a2eed04425b38c9af685d9172
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 05:16:50 -0500

    remove yaml file from build folder

--- server/swagger.js
+++ server/swagger.js
@@ -4,6 +4,6 @@ import yaml from "yamljs";
 import {resolve} from "path";
 
 export const setupSwagger = (app) => {
-    let swaggerJsDocs = yaml.load(resolve("./server", "api.yaml"));
+    let swaggerJsDocs = yaml.load(resolve("./", "api.yaml"));
 	app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
 }
