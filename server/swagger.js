23e1f7af69d6faf2fdd98532c42466c369a1193e
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 04:53:30 -0500

    path resolve

--- server/swagger.js
+++ server/swagger.js
@@ -1,7 +1,9 @@
+import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 import yaml from "yamljs";
+import {resolve} from "path";
 
 export const setupSwagger = (app) => {
-    let swaggerJsDocs = yaml.load('./api.yaml');
+    let swaggerJsDocs = yaml.load(resolve(process.env.NODE_ENV === "development" ? "./server" : "./", "api.yaml"));
 	app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
 }
