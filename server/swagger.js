bf3a9432e5be48e804acd22a3dc24e9784b8ab59
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 05:08:03 -0500

    path resolve issue

--- server/swagger.js
+++ server/swagger.js
@@ -4,6 +4,6 @@ import yaml from "yamljs";
 import {resolve} from "path";
 
 export const setupSwagger = (app) => {
-    let swaggerJsDocs = yaml.load(resolve(process.env.NODE_ENV === "development" ? "./server" : "./", "api.yaml"));
+    let swaggerJsDocs = yaml.load(resolve("./server", "api.yaml"));
 	app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
 }
