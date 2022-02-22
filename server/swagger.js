12174161bf3419e2bef1951f5a6d26c50904919a
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 04:08:28 -0500

    path resolve

--- server/swagger.js
+++ server/swagger.js
@@ -2,6 +2,6 @@ import swaggerUi from "swagger-ui-express";
 import yaml from "yamljs";
 
 export const setupSwagger = (app) => {
-    let swaggerJsDocs = yaml.load('server/api.yaml');
+    let swaggerJsDocs = yaml.load('api.yaml');
 	app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerJsDocs));
 }
