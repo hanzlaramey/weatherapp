29059f67712538af73fd1d974147958353783f1b
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 05:48:16 -0500

    removing react router link and out html a

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -22,9 +22,9 @@ export const LandingPage = () => {
 						<Link to="/register">
 							<Button variant="outline-light" className="m-2 mt-5 px-5  rounded-pill">Register</Button>
 						</Link>
-						<Link to={"/api/docs"}>
+						<a target="_blank" href="/api/docs">
 							<Button variant="outline-light" className="m-2 mt-5 px-5  rounded-pill">Swagger Documentation</Button>
-						</Link>
+						</a>
 					</Col>
 				</Row>
 			</Container>
