a516a5b71cf6c96a1a42563c69f54f1b50968861
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 06:59:30 -0500

    NavBar link bugFixe

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,13 @@ import { Navbar, Nav, Container} from 'react-bootstrap';
 import logo from "../assets/logo.png"
 
 export const NavBar = () => {
-	
+
 	return(
 		<Navbar bg="light" variant="light">
 			<Container>
 				<Navbar.Brand href="#home"><img src={logo} width="50px" alt="logo" /></Navbar.Brand>
 				<Nav className="ms-auto">
-					<Nav.Link to="/logout">Logout</Nav.Link>
+					<Nav.Link href="/logout">Logout</Nav.Link>
 				</Nav>
 			</Container>
 		</Navbar>
