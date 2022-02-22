08b17559c008305d675c59f194f89a31dcac116f
Hanzla Ramey <hanzlaramey4912@gmail.com>
2022-02-22 07:12:04 -0500

    NavBar Logout Production Fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container} from 'react-bootstrap';
 import logo from "../assets/logo.png"
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
 
@@ -9,7 +10,7 @@ export const NavBar = () => {
 			<Container>
 				<Navbar.Brand href="#home"><img src={logo} width="50px" alt="logo" /></Navbar.Brand>
 				<Nav className="ms-auto">
-					<Nav.Link href="/logout">Logout</Nav.Link>
+					<Link to="/logout" className="link-secondary bg-white" style={{textDecoration:"none"}}>Logout</Link>
 				</Nav>
 			</Container>
 		</Navbar>
